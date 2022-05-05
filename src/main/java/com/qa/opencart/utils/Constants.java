5612e725e6cd7fc47f4025af86016c14465a04a8
Satishjalluri <satishjalluri@gmail.com>
2022-05-05 07:44:07 +0530

    hot fix added to demo page title constant

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -5,6 +5,9 @@ public class Constants {
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String LOGIN_PAGE_FRACTION_URL = "route=account/login";
 	
+	public static final String DEMO_PAGE_TitLE = "Account Demo";
+	
+	
 	
 
 }
