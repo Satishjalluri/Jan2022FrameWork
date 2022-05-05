e850e8122675bc729beb43f4dca29cb74b6a08d2
Satishjalluri <103469056+Satishjalluri@users.noreply.github.com>
2022-05-05 08:08:10 +0530

    Create Sample.java

---
+++ src/main/java/com/qa/opencart/pages/Sample.java
@@ -0,0 +1,16 @@
+package com.qa.opencart.pages;
+
+public class Sample {
+	
+	public void login() {
+		
+		String username = "TOM";
+    String password = "Tom123"
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
