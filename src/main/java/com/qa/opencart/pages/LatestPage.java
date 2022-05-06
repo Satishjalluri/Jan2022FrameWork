f274b8ad949b76c6c4c71b15206e6d38024d2922
Satishjalluri <103469056+Satishjalluri@users.noreply.github.com>
2022-05-06 10:40:11 +0530

    Create LatestPage.java

---
+++ src/main/java/com/qa/opencart/pages/LatestPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class LatestPage {
+
+	public static void main(String[] args) {
+
+
+		String s = "Latest Page page";
+		System.out.println(s);
+		
+	}
+}
+		
